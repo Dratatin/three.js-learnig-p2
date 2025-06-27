e85ad02fae4d3968c042339a09a89e26e884e8a1
Dratatin <matchirouze@orange.fr>
2025-06-27 15:01:11 +0200

    Implement earth texture

--- main.js
+++ main.js
@@ -9,32 +9,49 @@ const canvasHeight = canvasElement.offsetHeight;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(canvasWidth, canvasHeight);
 canvasElement.appendChild(renderer.domElement);
-
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
 const fov = 75;
-const far = 10;
+const far = 1000;
 const near = .1;
 const ratio = canvasWidth/canvasHeight;
 const camera = new THREE.PerspectiveCamera(fov, ratio, near, far);
-camera.position.z = 5;
+camera.position.z = 4;
 const scene = new THREE.Scene();
 const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new THREE.TextureLoader();
 
 
-const cubeGeometry = new THREE.BoxGeometry();
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: "yellow"
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+
+
+const earthGeometry = new THREE.IcosahedronGeometry(1, 12);
+const earthMaterial = new THREE.MeshStandardMaterial({
+    map: loader.load("./texture/earthmap10k.jpg")
 });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
+const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+earthGroup.add(earthMesh);
+
+const earthLightMaterial = new THREE.MeshBasicMaterial({
+    map: loader.load("./texture/earthlights10k.jpg"),
+    blending: THREE.AdditiveBlending
+})
+const earthLightMesh = new THREE.Mesh(earthGeometry, earthLightMaterial);
+earthGroup.add(earthLightMesh);
 
 
-(function animate(t) {
+const sunLight = new THREE.DirectionalLight("white", 2.0);
+sunLight.position.set(-1,1,1);
+scene.add(sunLight);
+
+
+(function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.z = t * 0.0002;
-    cube.rotation.x = t * 0.0003;
+    earthMesh.rotation.y += 0.002;
+    earthLightMesh.rotation.y += 0.002;
     renderer.render(scene, camera);
 })()
-
-
 
