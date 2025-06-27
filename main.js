26d46346668d0e15ba8be2cd72de6ba3c10c9526
Dratatin <matchirouze@orange.fr>
2025-06-27 17:30:58 +0200

    Implement earth textures

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { getFresnelMat } from "./utils/getFresnelMat.js";
 
 const canvasElement = document.getElementById("canvas-element");
 const canvasWidth = canvasElement.offsetWidth;
@@ -13,13 +14,19 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
 const fov = 75;
-const far = 1000;
+const far = 10;
 const near = .1;
 const ratio = canvasWidth/canvasHeight;
 const camera = new THREE.PerspectiveCamera(fov, ratio, near, far);
 camera.position.z = 4;
 const scene = new THREE.Scene();
+
+
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = .003;
+
+
 const loader = new THREE.TextureLoader();
 
 
@@ -35,12 +42,30 @@ const earthMaterial = new THREE.MeshStandardMaterial({
 const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
 earthGroup.add(earthMesh);
 
+
 const earthLightMaterial = new THREE.MeshBasicMaterial({
     map: loader.load("./texture/earthlights10k.jpg"),
     blending: THREE.AdditiveBlending
 })
 const earthLightMesh = new THREE.Mesh(earthGeometry, earthLightMaterial);
 earthGroup.add(earthLightMesh);
+
+
+const earthCloudMaterial = new THREE.MeshStandardMaterial({
+    map: loader.load("./texture/earthhiresclouds4K.jpg"),
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    opacity: .7
+})
+const earthCloudMesh = new THREE.Mesh(earthGeometry, earthCloudMaterial);
+earthCloudMesh.scale.setScalar(1.005);
+earthGroup.add(earthCloudMesh);
+
+
+const earthGlowMaterial = getFresnelMat();
+const earthGlowMesh = new THREE.Mesh(earthGeometry, earthGlowMaterial);
+earthGlowMesh.scale.setScalar(1.008);
+earthGroup.add(earthGlowMesh);
 
 
 const sunLight = new THREE.DirectionalLight("white", 2.0);
@@ -50,8 +75,17 @@ scene.add(sunLight);
 
 (function animate() {
     requestAnimationFrame(animate);
-    earthMesh.rotation.y += 0.002;
-    earthLightMesh.rotation.y += 0.002;
+    earthMesh.rotation.y += 0.0005;
+    earthLightMesh.rotation.y += 0.0005;
+    earthCloudMesh.rotation.y += 0.0008;
+    controls.update();
     renderer.render(scene, camera);
 })()
 
+canvasElement.addEventListener("mousedown", () => {
+    canvasElement.classList.add("dragging");
+})
+
+canvasElement.addEventListener("mouseup", () => {
+    canvasElement.classList.remove("dragging");
+})
